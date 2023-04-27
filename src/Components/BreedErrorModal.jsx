5b12d533a50311cf48fe65d078bcd8770c49ab9f
jovylim <jovylimwenjing@gmail.com>
2023-04-27 22:17:40 +0800

    added error module for breeds data

---
+++ src/Components/BreedErrorModal.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./BreedErrorModal.module.css";
+
+const Overlay = (props) => {
+  return (
+    <div className={styles.backdrop}>
+      <div className={styles.modal}>
+        <div
+          className={styles.message}
+        >{`It's a cat-astrophe! The ${props.name} cat is missing!!!!`}</div>
+        <div className={styles.message}>
+          {`Jokes aside, since the data on this website belong to TheCatAPI, I have no control over the removal of any data. The data for the ${props.name} is no longer available. :(`}
+        </div>
+        <img src="../../pictures/sorrycat.jpeg" className={styles.sorry}></img>
+        <button
+          className={styles.close}
+          onClick={() => props.setShowBreedErrorModal(false)}
+        >
+          go look for other cats
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const BreedErrorModal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Overlay
+          name={props.name}
+          setShowBreedErrorModal={props.setShowBreedErrorModal}
+        />,
+        document.querySelector("#modal-root")
+      )}
+    </>
+  );
+};
+
+export default BreedErrorModal;
--- src/Components/BreedListItem.jsx
+++ src/Components/BreedListItem.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import styles from "./BreedListItem.module.css";
 import { useState } from "react";
 import BreedListItemModal from "./BreedListItemModal";
+import BreedErrorModal from "./BreedErrorModal";
 
 const BreedListItem = (props) => {
   const [showBreedListItemModal, setShowBreedListItemModal] = useState(false);
+  const [showBreedErrorModal, setShowBreedErrorModal] = useState(false);
   const [breedData, setBreedData] = useState([]);
 
   const getBreedData = async () => {
     const res = await fetch(
       import.meta.env.VITE_SERVER + "&breed_ids=" + props.id
     );
-    const cat = await res.json();
-    setBreedData(cat);
-    setShowBreedListItemModal(true);
+
+    if (res.status === 200) {
+      const cat = await res.json();
+
+      if (cat.length === 0) {
+        setShowBreedErrorModal(true);
+      } else {
+        console.log("info found");
+        setBreedData(cat);
+        setShowBreedListItemModal(true);
+      }
+    }
   };
 
   return (
@@ -24,6 +35,14 @@ const BreedListItem = (props) => {
           setShowBreedListItemModal={setShowBreedListItemModal}
         />
       )}
+
+      {showBreedErrorModal && (
+        <BreedErrorModal
+          name={props.name}
+          setShowBreedErrorModal={setShowBreedErrorModal}
+        />
+      )}
+
       <div className={styles.breed} onClick={() => getBreedData()}>
         {props.name}
       </div>
